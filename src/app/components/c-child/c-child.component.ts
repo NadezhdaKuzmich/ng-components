ffce4ff8d5a043c70785fa3ec3ee2eb8935c7fb3
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-01-24 23:50:35 +0100

    add new sections

--- src/app/components/c-child/c-child.component.ts
+++ src/app/components/c-child/c-child.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, ContentChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-c-child',
   templateUrl: './c-child.component.html',
-  styleUrl: './c-child.component.scss'
+  styleUrl: './c-child.component.scss',
 })
 export class CChildComponent {
+  @Input() flag = false;
+  
+  @ContentChild('elem') elem: ElementRef | undefined;
 
+  changeText() {
+    if (this.elem) {
+      this.elem.nativeElement.innerHTML = 'Changed from Content Child';
+    }
+  }
 }
--- src/app/components/c-parent/c-parent.component.ts
+++ src/app/components/c-parent/c-parent.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-c-parent',
   templateUrl: './c-parent.component.html',
-  styleUrl: './c-parent.component.scss'
+  styleUrl: './c-parent.component.scss',
 })
 export class CParentComponent {
-
+  parentText = 'Parent text';
 }
